a5d1f04b5bb81d7c5d7f01ed8397c9702673cb67
SEOYOON <insummer712@gmail.com>
2023-05-18 15:46:26 +0900

    점수 계산

--- src/main/java/kr/ac/sahmyook/HangmanScore.java
+++ src/main/java/kr/ac/sahmyook/HangmanScore.java
@@ -1,18 +1,17 @@
 package kr.ac.sahmyook;
 
 public class HangmanScore {
-    public static void count(int count) {
+    public static void count(int fail) {
 
-
-        if (count >= 10) {
+        if (fail >= 10) {
             System.out.println("0점 입니다.");
-        } else if (count >= 8) {
+        } else if (fail >= 8) {
             System.out.println("60점 입니다.");
-        } else if (count >= 6) {
+        } else if (fail >= 6) {
             System.out.println("70점 입니다.");
-        } else if (count >= 4) {
+        } else if (fail >= 4) {
             System.out.println("80점 입니다.");
-        } else if (count >= 2) {
+        } else if (fail >= 2) {
             System.out.println("90점 입니다.");
         } else {
             System.out.println("100점 입니다!!!");
